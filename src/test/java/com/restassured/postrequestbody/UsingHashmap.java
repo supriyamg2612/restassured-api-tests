04deb5d611a8d6db953b7884817cc8f6688a9f4f
Supriya Ghuge <supriyamg2612@users.noreply.github.com>
2025-09-16 17:20:38 +0200

    Refactor: add static imports for Rest Assured methods

--- src/test/java/com/restassured/postrequestbody/UsingHashmap.java
+++ src/test/java/com/restassured/postrequestbody/UsingHashmap.java
@@ -1,5 +1,13 @@
 package com.restassured.postrequestbody;
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
 
 public class UsingHashmap {
+	
+	void testUsingHashamp() {
+		
+	}
 
 }
