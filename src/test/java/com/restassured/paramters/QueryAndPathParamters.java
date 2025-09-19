0d21824933ca4c0174466fea21de55e8c82cabb3
Supriya Ghuge <supriyamg2612@users.noreply.github.com>
2025-09-19 15:28:20 +0200

    feat: add QueryAndPathParamters test with query and path parameter example

--- src/test/java/com/restassured/paramters/QueryAndPathParamters.java
+++ src/test/java/com/restassured/paramters/QueryAndPathParamters.java
@@ -1,6 +1,32 @@
 package com.restassured.paramters;
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+import org.testng.annotations.Test;
 
 public class QueryAndPathParamters {
+	
+	//  https://reqres.in/api/users?page=2
+	
+	@Test
+	void testQueryAndPathParamters() {
+		
+		given()
+		.pathParam("mypath", "users") //path parameter
+		.queryParam("page", 2) // Query parameter
+		
+		
+		
+		.when()
+		.get("https://reqres.in/api/{mypath}")
+		
+		
+		.then()
+		.statusCode(200)
+		.log().all();
+			
+	}
 	
 	
 
